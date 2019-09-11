789b94d806482d7abb6c58a1f181cccfdc7c5120
irauschert <54558637+irauschert@users.noreply.github.com>
2019-09-10 23:55:28 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "ines" && password == "12345678") {
+        window.location = "portada.html"
+        return false;
+    }
+    else {
+        alert("Usuario o contraseña incorectos, aunque te vamos a dejar entrar igual :)");
+        window.location = "portada.html"
+        return false;
+    }
+}
